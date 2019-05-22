29930f89c9bffeabd59f0f50fb9a7284a1a5ac35
Fabiow Quixadá <fabio.quixada@osf-commerce.com>
2019-05-22 10:52:39 -0300

    Code Changes

---
+++ osflinter.config.js
@@ -0,0 +1,15 @@
+module.exports.stylelintPaths = [
+    "cartridges/app_demo/cartridge/client/*/css/**/*.scss"
+];
+
+module.exports.eslintClientPaths = [
+    "cartridges/app_demo/cartridge/client/*/js/**/*.js"
+];
+
+module.exports.eslintServer = [
+    "cartridges/app_demo/cartridge/client/*/js/**/*.js"
+];
+
+module.exports.ismlLinter = [
+    "cartridges/**/templates/**/*.isml"
+];
