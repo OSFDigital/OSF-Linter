aa6911738e2cb854664ef768a4cdd0e4f0641e4c
Daniel Anechitoaie <10779010+danechitoaie@users.noreply.github.com>
2019-05-30 10:59:31 +0300

    Create config.js

---
+++ ismllint/config.js
@@ -0,0 +1,12 @@
+const config = {
+    "rootDir": "./cartridges",
+    "ignoreUnparseable": true,
+    "disableTreeParse": false,
+    "rules": {
+        "no-space-only-lines": {},
+        "no-tabs": {},
+        "no-trailing-spaces": {}
+    }
+};
+
+module.exports = config;
