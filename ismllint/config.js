0ba5ebde5fc59c4d211a2d6bd7cf82d7336bc83e
Daniel Anechitoaie <10779010+danechitoaie@users.noreply.github.com>
2019-05-30 11:20:17 +0300

    Update config.js

--- ismllint/config.js
+++ ismllint/config.js
@@ -1,4 +1,4 @@
-const config = {
+let ismllintConfig = {
     "rootDir": "./cartridges",
     "ignoreUnparseable": true,
     "disableTreeParse": false,
@@ -9,4 +9,34 @@ const config = {
     }
 };
 
-module.exports = config;
+const chalk = require("chalk");
+const merge = require("deepmerge");
+const fse = require("fs-extra");
+const path = require("path");
+const process = require("process");
+
+let osfLinterConfigPath = path.resolve(process.cwd(), "osflinter.config.js");
+if (!fse.existsSync(osfLinterConfigPath)) {
+    console.error(`${chalk.red.bold("\u2716")} ${osfLinterConfigPath} does not exist!`);
+    process.exit(1);
+}
+
+let osfLinterConfig;
+try {
+    osfLinterConfig = require(osfLinterConfigPath);
+} catch (e) {
+    console.error(`${chalk.red.bold("\u2716")} Failed to import ${osfLinterConfigPath}!`);
+    console.error(e);
+    process.exit(1);
+}
+
+if (!osfLinterConfig) {
+    console.error(`${chalk.red.bold("\u2716")} Failed to import ${osfLinterConfigPath}!`);
+    process.exit(1);
+}
+
+if (osfLinterConfig.ismllintConfig) {
+    ismllintConfig = merge(ismllintConfig, osfLinterConfig.ismllintConfig);
+}
+
+module.exports = ismllintConfig;
