caeabf3a628ab77b4c2fb2669d1c4093c8e1387b
ClaudiuCudla <claudiu.cudla@osf-commerce.com>
2019-06-25 11:24:20 +0300

    Updated eslint rules for comma spacing
    Updated eslint rules to properly have spacing on enumeration commas

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -9,9 +9,12 @@ const defaultConfig = {
     },
     globals: {},
     rules: {
+        "array-bracket-spacing": ["error", "never"],
+        "comma-spacing": ["error", { before: false, after: true }],
         "eol-last": ["error", "always"],
         "func-style": "off",
         "global-require": "off",
+        "indent": ["error", 4, { SwitchCase: 1 }],
         "keyword-spacing": ["error", { before: true, after: true }],
         "linebreak-style": ["error", "unix"],
         "no-bitwise": "off",
@@ -20,12 +23,13 @@ const defaultConfig = {
         "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
         "prefer-const": "off",
         "prefer-spread": "off",
+        "quotes": ["error", "double"],
+        "radix": ["error", "always"],
+        "semi": ["error", "always"],
+        "space-before-blocks": ["error", "always"],
+        "space-before-function-paren": ["error", { anonymous: "always", named: "never", asyncArrow: "always"}],
         "space-in-parens": ["error", "never"],
-        indent: ["error", 4, { SwitchCase: 1 }],
-        quotes: ["error", "double"],
-        radix: ["error", "always"],
-        semi: ["error", "always"],
-        strict: ["error", "global"]
+        "strict": ["error", "global"]
     }
 };
 
