831cfeab19a13295c8a34db094d945f728889b31
Fabio Quixadá <48183626+fabioquixadaosf@users.noreply.github.com>
2020-07-27 05:30:44 -0300

    Update Isml Linter (#29)
    * Update Isml Linter
    
    * Remove Isml Linter Rules

--- config/.ismllintrc.js
+++ config/.ismllintrc.js
@@ -2,12 +2,10 @@ module.exports = {
     enableCache: true,
     rules: {
         // Line by line rules;
-        "enforce-isprint": {},
         "enforce-require": {},
         "no-git-conflict": {},
         "no-import-package": {},
         "no-inline-style": {},
-        "no-isscript": {},
         "no-space-only-lines": {},
         "no-tabs": {},
         "no-trailing-spaces": {},
